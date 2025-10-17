9906f56f7b069af5970058325fac10c53d831813
Incerteza88 <info@incerteza.es>
2025-10-17 09:33:11 +0000

    login trabajador ruta

--- src/api/rutas/ruta_login.py
+++ src/api/rutas/ruta_login.py
@@ -2,6 +2,7 @@ from flask import Blueprint, request, jsonify
 from api.models import Cliente, Trabajador
 from flask_jwt_extended import create_access_token  
 import bcrypt
+from datetime import timedelta
 
 login = Blueprint("login", __name__, url_prefix="/login")
 
@@ -27,10 +28,48 @@ def login_cliente():
     #TOKEN JWT
     token =create_access_token(
         identity={"id": cliente.id, "rol": "cliente"}
-        expires_delta=timedelta(hours=1)
+        expires_delta=timedelta(hours=2)
     )
     
     return jsonify({
+        "msg": "Login exitoso",
         "token": token,
         "cliente": cliente.to_dict()
     }), 200
+
+
+# LOGIN TRABAJADOR
+
+@login.route("/trabajador", method=["POST"])
+def login_trabajador():
+    data = request.get.json()
+    email = data.get("email")
+    password = data.get("password")
+
+    if not email or password:
+        return jsonify({"error": "Email y contraseña son requeridos"}), 400
+    
+    trabajador = Trabajador.query.filter_by(email=email).first()
+    if not trabajador:
+        return jsonify({"error": "Trabajador no encontrado"}), 404
+    
+    #Contraseña
+    if not bcrypt.checkw(password.encode("utf-8"), trabajador.password.encode("utf-8")):
+        return jsonify({"error": "Contraseña incorrecta"}),401
+    
+    # TOKEN
+
+    token = create_access_token(
+        identity={"id": trabajador.id, "rol": "trabajador"},
+        expires_delta=timedelta(hours=2)
+    )
+    
+    return jsonify({
+        "msg": "Login exitoso",
+        "token": token,
+        "cliente": trabajador.to_dict()
+    }), 200
+
+    
+
+    
